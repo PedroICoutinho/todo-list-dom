6d869660c4700e25f931ab93899f77d8e38a9312
Pedro Ivo <pedroooivo@hotmail.com>
2023-03-29 20:22:08 -0300

    Jogo da forca

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
-const inputTask = document.getElementById("input-task");
-const btnAddTask = document.getElementById("btn-add-task");
-const taskBox = document.getElementById("task-box");
-const ulPlaceholder = document.getElementById("ul-placeholder");
-const checkboxTasks = document.getElementsByClassName("checkbox-tasks");
+const curso = ["IronHacker", "Hacker", "Iron"];
 
-btnAddTask.addEventListener("click", () => {
-  if (taskBox.children[0] === ulPlaceholder) {
-    taskBox.removeChild(ulPlaceholder);
+const palavraSecreta = curso[Math.floor(Math.random() * curso.length)];
+
+document.addEventListener("keydown", (evento)=>{
+  const codigo = evento.code;
+  if(isLetra(codigo)){
+    const letra = evento.key
   }
 
-  const task = document.createElement("li");
 
-  const checkbox = document.createElement("input");
-  checkbox.setAttribute("type", "checkbox");
-  checkbox.setAttribute("class", "checkbox-tasks");
+})
 
-  checkbox.addEventListener("change", () => {
-    task.classList.toggle("task-done");
-  });
 
-  task.innerText = inputTask.value;
-  task.appendChild(checkbox);
-
-  taskBox.appendChild(task);
-
-  inputTask.value = "";
-
-  // Tirar o P de dentro da taskbox (SE TIVER) CHECK!
-  // Criar uma LI CHECK!
-  // Colocar dentro da LI o value do input CHECK!
-  // Colocar a LI dentro da Taskbox CHECK!
-});
+function isLetra(codigo){
+  return codigo >= 60 && codigo <= 90;
+}
