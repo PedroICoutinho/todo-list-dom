f28d65d7dbbb8f9ac036a87eddd9e944e96894c6
Pedro Ivo <pedroooivo@hotmail.com>
2023-03-30 16:00:33 -0300

    jogo da forca

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 const curso = ["IronHacker", "Hacker", "Iron"];
-
 const palavraSecreta = curso[Math.floor(Math.random() * curso.length)];
+
+const palavraSeparada = palavraSecreta.split("")
+const letrasCorretas = []
+const letrasErradas = []
+
 
 document.addEventListener("keydown", (evento)=>{
   const codigo = evento.code;
   if(isLetra(codigo)){
     const letra = evento.key
-  }
-
-
+    if(palavraSeparada.includes(letra)){
+      letrasCorretas.push(letra)
+    }else {
+      letrasErradas.push(letra)
+    }
+  
+  } 
+  atualizarJogo()
 })
+
+function atualizarJogo(){
+  mostrarLetrasErradas();
+  mostrarLetrasCorretas();
+}
+
+function mostrarLetrasCorretas(){
+  const certas = document.getElementsByClassName("palavra-secreta-container")
+  letrasCorretas.forEach(letras =>{
+    certas.innerHTML += ` ${letras} `
+  })
+}
+
+function mostrarLetrasErradas(){
+  const erradas = document.getElementsByClassName("letras-erradas-container")
+  letrasErradas.forEach(letra => {
+    erradas.innerHTML += ` ${letra} `
+  })
+}
 
 
 function isLetra(codigo){
-  return codigo >= 60 && codigo <= 90;
+  return codigo >= 65 && codigo <= 90;
 }
